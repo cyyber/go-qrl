511abf1b2665512b55b752a9e4eedeb96938c0f8
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-14 13:34:14 +0400

    Review files

--- qrlclient/qrlclient_test.go
+++ qrlclient/qrlclient_test.go
@@ -181,7 +181,7 @@ func TestToFilterArg(t *testing.T) {
 }
 
 var (
-	testWallet, _ = wallet.Generate(wallet.ML_DSA_87)
+	testWallet, _ = wallet.RestoreFromSeedHex("010000f29f58aff0b00de2844f7e20bd9eeaacc379150043beeb328335817512b29fbb7184da84a092f842b2a06d72a24a5d28")
 	testAddr      = testWallet.GetAddress()
 	testBalance   = big.NewInt(2e15)
 )
