d75e62da6a4ec9169538ed638ca82eaeee5ffd26
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-14 12:46:59 +0400

    Review files

--- accounts/abi/bind/backends/simulated_test.go
+++ accounts/abi/bind/backends/simulated_test.go
@@ -97,7 +97,7 @@ func TestSimulatedBackend(t *testing.T) {
 	}
 }
 
-var testWallet, _ = wallet.RestoreFromSeedHex("0x010000b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+var testWallet, _ = wallet.RestoreFromSeedHex("0x010000f29f58aff0b00de2844f7e20bd9eeaacc379150043beeb328335817512b29fbb7184da84a092f842b2a06d72a24a5d28")
 
 // the following is based on this contract:
 //
