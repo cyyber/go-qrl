237812569cca91e027b02c6b2c26d10657001a7f
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-14 13:20:49 +0400

    Review files

--- cmd/devp2p/internal/qrltest/transaction.go
+++ cmd/devp2p/internal/qrltest/transaction.go
@@ -27,13 +27,13 @@ import (
 
 	"github.com/theQRL/go-zond/common"
 	"github.com/theQRL/go-zond/core/types"
-	"github.com/theQRL/go-zond/crypto/pqcrypto"
+	"github.com/theQRL/go-zond/crypto/pqcrypto/wallet"
 	"github.com/theQRL/go-zond/internal/utesting"
 	"github.com/theQRL/go-zond/params"
 )
 
 // var faucetAddr, _ = common.NewAddressFromString("Q71562b71999873DB5b286dF957af199Ec94617F7")
-var faucetKey, _ = pqcrypto.WalletFromHexSeed("010000b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
+var faucetKey, _ = wallet.RestoreFromSeedHex("0x010000f29f58aff0b00de2844f7e20bd9eeaacc379150043beeb328335817512b29fbb7184da84a092f842b2a06d72a24a5d28")
 
 func (s *Suite) sendSuccessfulTxs(t *utesting.T) error {
 	tests := []*types.Transaction{
