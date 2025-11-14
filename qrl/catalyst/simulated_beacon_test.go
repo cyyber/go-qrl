94a9e36ee4e7f81daa15c7dd5b0c306ddf59d069
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2025-11-14 18:30:40 +0400

    qrl/catalyst/simulated_beacon_test.go: review file

--- qrl/catalyst/simulated_beacon_test.go
+++ qrl/catalyst/simulated_beacon_test.go
@@ -78,7 +78,7 @@ func TestSimulatedBeaconSendWithdrawals(t *testing.T) {
 
 	var (
 		// testKey is a wallet to use for funding a tester account.
-		testWallet, _ = wallet.RestoreFromSeedHex("010000f29f58aff0b00de2844f7e20bd9eeaacc379150043beeb328335817512b29fbb7184da84a092f842b2a06d72a24a5d28")
+		testWallet, _ = wallet.RestoreFromSeedHex("010000b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f29100000000000000000000000000000000")
 
 		// testAddr is the QRL address of the tester account.
 		testAddr = testWallet.GetAddress()
