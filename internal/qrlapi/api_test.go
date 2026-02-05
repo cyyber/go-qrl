ac741c70c2c30f6ee51ae030d34cd856f9ba2308
Ricardo Geraldes <144315960+rgeraldes24@users.noreply.github.com>
2026-02-05 18:56:30 +0400

    Revert change

--- internal/qrlapi/api_test.go
+++ internal/qrlapi/api_test.go
@@ -275,9 +275,6 @@ func (b testBackend) BlockByNumber(ctx context.Context, number rpc.BlockNumber) 
 	if number == rpc.PendingBlockNumber {
 		return b.pending, nil
 	}
-	if number == rpc.EarliestBlockNumber {
-		number = 0
-	}
 	return b.chain.GetBlockByNumber(uint64(number)), nil
 }
 func (b testBackend) BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error) {
